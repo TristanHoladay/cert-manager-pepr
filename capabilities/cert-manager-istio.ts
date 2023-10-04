4068c809a6dd0d63d1d3884d244ff86fcb1d5506
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-10-04 09:41:26 -0600

    abstracted gateway patch logic; added handling of tenant secret creation.

--- capabilities/cert-manager-istio.ts
+++ capabilities/cert-manager-istio.ts
@@ -1,4 +1,4 @@
-import { Capability, a, K8s } from "pepr";
+import { Capability, a, K8s, Log } from "pepr";
 
 export const CertManagerIstio = new Capability({
   name: "cert-manager-istio",
@@ -9,44 +9,38 @@ export const CertManagerIstio = new Capability({
 
 const { When } = CertManagerIstio;
 
-class Gateway extends a.GenericKind {}
-
-When(a.Secret)
-  .IsCreated()
-  .WithLabel("app.kubernetes.io/istio-gateway-secret", "admin")
-  .Mutate(async secret => {
-    await K8s(a.GenericKind, {
-      kindOverride: {
-        kind: "gateways",
-        group: "networking.istio.io",
-        version: "v1beta1",
-      },
-      name: "admin",
-      namespace: "istio-system",
-    }).Patch([
+async function patchGateway(gateway: string, secretName: string) {
+  await K8s(a.GenericKind, {
+    kindOverride: {
+      kind: "gateways",
+      group: "networking.istio.io",
+      version: "v1beta1",
+    },
+    name: gateway,
+    namespace: "istio-system",
+  })
+    .Patch([
       {
         op: "replace",
         path: "/spec/servers/1/tls/credentialName",
-        value: secret.Raw.metadata.name,
+        value: secretName,
       },
-    ]);
+    ])
+    .catch(e => Log.error(e));
+}
 
-    // await ck8s.patchNamespacedCustomObject(
-    //   "networking.istio.io",
-    //   "v1beta1",
-    //   "istio-system",
-    //   "gateways",
-    //   "admin",
-    //   [
-    //     {
-    //       op: "replace",
-    //       path: "/spec/servers/1/tls/credentialName",
-    //       value: secret.Raw.metadata.name,
-    //     },
-    //   ],
-    //   undefined,
-    //   undefined,
-    //   undefined,
-    //   { headers: { "content-type": "application/json-patch+json" } },
-    // );
+When(a.Secret)
+  .IsCreated()
+  .InNamespace("istio-system")
+  .WithLabel("app.kubernetes.io/istio-gateway-secret", "admin")
+  .Mutate(async secret => {
+    patchGateway("admin", secret.Raw.metadata.name);
+  });
+
+When(a.Secret)
+  .IsCreated()
+  .InNamespace("istio-system")
+  .WithLabel("app.kubernetes.io/istio-gateway-secret", "tenant")
+  .Mutate(async secret => {
+    await patchGateway("tenant", secret.Raw.metadata.name);
   });
